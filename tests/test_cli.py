365e18e5cc83a0526f2cef92fd73aa739d09096c
Sean Gillies <sean.gillies@gmail.com>
2016-09-18 21:50:53 +0200

    Better test assertions

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -9,6 +9,11 @@ def test_cli_count():
     runner = CliRunner()
     result = runner.invoke(main_group, ['taxa'], input=data)
     assert result.exit_code == 0
-    assert '{"properties": {"aqueduct": "str"}, "geometry": "LineString"}' in result.output
-    assert '{"properties": {"architect": "str", "name": "str"}, "geometry": "Polygon"}' in result.output
-    assert '{"properties": {"name": "str"}, "geometry": "Point"}' in result.output
+    assert ('{"properties": {"aqueduct": "str"}, "geometry": "LineString"}' in result.output
+            or '{"geometry": "LineString", "properties": {"aqueduct": "str"}}' in result.output)
+    assert ('{"properties": {"architect": "str", "name": "str"}, "geometry": "Polygon"}' in result.output
+            or '{"geometry": "Polygon", "properties": {"architect": "str", "name": "str"}}' in result.output
+            or '{"properties": {"name": "str", "architect": "str"}, "geometry": "Polygon"}' in result.output
+            or '{"geometry": "Polygon", "properties": {"name": "str", "architect": "str"}}' in result.output)
+    assert ('{"properties": {"name": "str"}, "geometry": "Point"}' in result.output
+            or '{"geometry": "Point", "properties": {"name": "str"}}' in result.output)
